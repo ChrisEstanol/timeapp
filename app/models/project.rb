98cc9cdf7b14b6e258ea46c44c75f74f97a39c3f
Christophe E <cestanol@gmail.com>
2014-12-22 21:16:40 -0500

    Create couter models activity and project

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,14 @@ class Project < ActiveRecord::Base
   has_many :activities
 
   validates :name, presence: true
+
+  def total_time
+    times = Project.joins(:activities).where("activities.project_id" => id).pluck(:started_at, :ended_at)
+      if times.any?
+        times.map! do |x|
+          x[1] == nil ? 0 : x[1].to_time - x[0].to_time
+        end
+        p "#{times.inject(:+).round} segundos"
+      end
+    end
 end
